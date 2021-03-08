e9a5c9f8f9f941cd848efec5d2274a23cfa6f623
Ondrej <panicek.ondrej@gmail.com>
2021-03-08 17:20:47 +0100

    pridana responzivita, responzivna navigacia, scroll animacia

--- JS/script.js
+++ JS/script.js
@@ -9,8 +9,27 @@ $("html, body").animate({ scrollTop: $(this.hash).offset().top}, 500);
 event.preventDefault();
 });
 
+// back to top-----------------------------------------------
+var backToTop = $("<a>", {
+  href: "#",
+  class: "back-to-top",
+  html: `<i class="fas fa-arrow-up"></i>`
+});
+
+backToTop
+.hide()
+.appendTo("body")
+.on("click", function() {
+$("html, body").animate({scrollTop: 0});
+});
+var win = $(window);
+win.on("scroll", function(){
+if (win.scrollTop() >= 750 ) backToTop.fadeIn();
+else backToTop.fadeOut();
+});
+
 // Lightbox for gallery----------------------------
-var gallery = $('.gallery')
+var gallery = $('.slider')
 
   var overlay = $("<div/>",  {id: "overlay" });
       overlay
@@ -35,4 +54,40 @@ var gallery = $('.gallery')
     $(document).on('keyup', function(event){
      if (event.which === 27) overlay.fadeOut();
     });
-})(jQuery);
+
+// Responsive navigation---------------------------
+var navCover = $('#nav-cover')
+    navCoverLinks = $('#nav-cover a')
+    navCover.hide();
+
+    $('.nav-icon').click(function() {
+      var navCover = $('#nav-cover')
+          navIcon = $('.nav-icon i')
+
+      navCover.slideToggle(200);
+      switchClass();
+
+    });
+    navCoverLinks.on('click', function(){
+      navCover.slideToggle();
+      switchClass();
+    });
+
+    function switchClass() {
+      if (navIcon.hasClass('fa fa-bars')) {
+        navIcon.addClass('fas fa-times');
+        navIcon.removeClass('fa fa-bars');
+      } else {
+        navIcon.addClass('fa fa-bars');
+        navIcon.removeClass('fas fa-times');
+      }
+    }
+
+// Gallery slider-------------------------------
+
+var slider = $('.slider'),
+    sliderImg = $('.slider-img');
+
+
+
+  })(jQuery);
